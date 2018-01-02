14818eab61626dd2f9c684ba2bd95b71d4d9e4c4
ErickAlv <34917844+ErickAlv@users.noreply.github.com>
2018-01-02 01:50:49 +0100

    v

---
+++ Game/Assets/Scripts/Box.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+    Rigidbody m_box;
+
+    // Use this for initialization
+    void Start () {
+        m_box = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            m_box.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
+            m_box.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationY;
+        }
+    }
+}
