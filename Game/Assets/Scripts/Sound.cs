4471bd3a470c5f9b7d49cfba89842fd60ca74129
ErickAlv <34917844+ErickAlv@users.noreply.github.com>
2018-01-09 00:39:48 +0100

    actualization

---
+++ Game/Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound{
+
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f,1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    //For reproducin a sound via code in a specific moment
+    //FindObjectOfType<AudiManager>().Play("NameOfTheSoundInInspector");
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
